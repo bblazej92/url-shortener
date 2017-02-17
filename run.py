0d3e34ea2cb08827ec9637364286076db8007927
Bartosz Błażejewski <bartosz.blazejewski@daftcode.pl>
2017-02-17 15:03:48 +0100

    Creating basic development environment in docker compose

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import click
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,5 +9,13 @@ def hello_world():
     return 'Hello world!'
 
 
+@click.command()
+@click.option('--host', type=click.STRING, default='0.0.0.0')
+@click.option('--port', type=click.INT, default='5000')
+def main(port, host):
+    app.run(host=host, port=port)
+
+
 if __name__ == '__main__':
-    app.run()
+    main()
+
