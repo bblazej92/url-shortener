d266436b5e7eea3b4e574d8e3f08a653091fce42
Bartosz Błażejewski <bartosz.blazejewski@daftcode.pl>
2017-02-17 16:31:47 +0100

    Better structure of application

--- run.py
+++ run.py
@@ -1,21 +1,15 @@
 import click
-from flask import Flask
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+from app import create_app
 
 
 @click.command()
 @click.option('--host', type=click.STRING, default='0.0.0.0')
 @click.option('--port', type=click.INT, default='5000')
 def main(port, host):
+    app = create_app()
     app.run(host=host, port=port)
 
 
 if __name__ == '__main__':
     main()
-
