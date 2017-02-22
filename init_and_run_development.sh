197d8cfde5a59998baf14ae5ae242d9fdf215bd0
Bartosz <bblazej92@gmail.com>
2017-02-22 01:59:07 +0100

    Implementing script to automated init and run development environment + writing scratch of README

---
+++ init_and_run_development.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+YELLOW='\033[1;33m'
+RESET='\033[0m'
+
+echo -e "${YELLOW}Generating virtualenv...${RESET}"
+source `which virtualenvwrapper.sh`
+mkvirtualenv url -a . --python=/usr/bin/python3.4
+
+echo -e "${YELLOW}Installing requirements...${RESET}"
+pip3 install -r requirements.txt
+sudo pip install docker-compose==1.8.0
+
+echo -e "${YELLOW}Building docker - development environment...${RESET}"
+cd docker/development
+sudo docker-compose build
+
+echo -e "${YELLOW}Running url-shortener server and mongodb in detached mode...${RESET}"
+sudo docker-compose up -d
+
+echo -e "${YELLOW}Running unit tests!${RESET}"
+cdproject
+python3 -m pytest
